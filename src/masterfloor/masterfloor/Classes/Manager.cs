3201acab70664bb93de8f1db4bd58f0db0264b25
Варвара <rrr@outlook.com>
2024-11-13 21:34:04 +0300

    creating a database and app interface

---
+++ src/masterfloor/masterfloor/Classes/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace masterfloor.Classes
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+        public static Data.partners_import currentPartners { get; set; }
+    }
+}
---
+++ src/masterfloor/masterfloor/Pages/ViewPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace masterfloor.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewPage.xaml
+    /// </summary>
+    public partial class ViewPage : Page
+    {
+        public ViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DiscountTextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

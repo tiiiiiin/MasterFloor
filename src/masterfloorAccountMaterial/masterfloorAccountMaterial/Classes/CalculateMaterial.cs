9c5539333740122957cf1d6706218d15bf684d8c
Варвара <rrr@outlook.com>
2024-11-13 23:36:07 +0300

    create modul 4 calculate material

---
+++ src/masterfloorAccountMaterial/masterfloorAccountMaterial/Classes/CalculateMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masterfloorAccountMaterial.Classes
+{
+    public class CalculateMaterial
+    {
+        public int Calculate(int IdTypeProduct, int IdTypeOfMaterial, int CountOFProducts,
+            double param1, double param2)
+        {
+            try
+            {
+                double counOn1 = param1 * param2 * Convert.ToDouble(Data.masterFloorEntities.GetContext().product_type_import.Where(d => d.Id == IdTypeProduct).FirstOrDefault().CoefProduct);
+                counOn1 += (param1 * param2 * Convert.ToDouble(Data.masterFloorEntities.GetContext().product_type_import.Where(d => d.Id == IdTypeProduct).FirstOrDefault().CoefProduct))
+                    * Convert.ToDouble(Data.masterFloorEntities.GetContext().Material_type_import.Where(d => d.Id == IdTypeOfMaterial).FirstOrDefault().PercenMarriag);
+
+                return Convert.ToInt32(Math.Round(counOn1 * CountOFProducts));
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ src/masterfloorAccountMaterial/masterfloorAccountMaterial/Program.cs
@@ -0,0 +1,20 @@
+﻿using masterfloorAccountMaterial.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masterfloorAccountMaterial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateMaterial calculateMaterial = new CalculateMaterial();
+            Console.WriteLine(calculateMaterial.Calculate(1, 1, 3, 12.8, 13.9));
+
+            Console.ReadKey();
+        }
+    }
+}
